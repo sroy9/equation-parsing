515c78e5eea39950d5ecdf2f17d6ded0a0950498
Subhro  Roy <subhroroy@Subhros-MacBook-Pro.local>
2014-12-12 10:11:42 -0600

    C

--- src/main/java/equationmatch/EquationFeatureExtractor.java
+++ src/main/java/equationmatch/EquationFeatureExtractor.java
@@ -182,9 +182,6 @@ public class EquationFeatureExtractor extends AbstractFeatureGenerator implement
 			for(String feature : FeatureExtraction.getMixed(blob.ta, blob.posTags, pos, 2)) {
 				features.add(prefix+"_Neighbors_"+feature);
 			}
-//			for(String feature : blob.ta.getSentenceFromToken(pos).getTokens()) {
-//				features.add(prefix+"_Sentence_"+feature);
-//			}
 		}
 		if(eqNo > 0) {
 			for(Pair<Operation, Double> pair : lattice.equations.get(0).B2) {
@@ -237,9 +234,9 @@ public class EquationFeatureExtractor extends AbstractFeatureGenerator implement
 			for(String feature : FeatureExtraction.getMixed(blob.ta, blob.posTags, pos, 2)) {
 				features.add(prefix+"_Neighbors_"+feature);
 			}
-			for(String feature : blob.ta.getSentenceFromToken(pos).getTokens()) {
-				features.add(prefix+"_Sentence_"+feature);
-			}
+//			for(String feature : blob.ta.getSentenceFromToken(pos).getTokens()) {
+//				features.add(prefix+"_Sentence_"+feature);
+//			}
 		}
 		if(eqNo > 0) {
 			for(Pair<Operation, Double> pair : lattice.equations.get(0).A2) {
