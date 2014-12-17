69dd330c0071f70b3a8a0a8ec662d5eb376ab045
Subhro  Roy <subhroroy@Subhros-MacBook-Pro.local>
2014-12-16 20:26:56 -0600

    C

--- src/main/java/equationmatch/EquationFeatureExtractor.java
+++ src/main/java/equationmatch/EquationFeatureExtractor.java
@@ -232,7 +232,7 @@ public class EquationFeatureExtractor extends AbstractFeatureGenerator implement
 		List<IntPair> spans = getRelevantSpans(blob, arrayName, d);
 		if(spans.size() == 0) {
 			System.out.println("Number not found : "+d);
-			System.out.println("Text : "+blob.ta);
+			System.out.println(blob.simulProb.index+" :Text : "+blob.ta);
 			System.out.println("Quantities : "+Arrays.asList(blob.quantities));
 		}
 		int pos = blob.ta.getTokenIdFromCharacterOffset(spans.get(0).getFirst());
