b6b87694a9a1217981129577b8298e411c0ef460
Subhro  Roy <subhroroy@Subhros-MacBook-Pro.local>
2014-12-16 21:35:01 -0600

    C

--- src/main/java/parser/DocReader.java
+++ src/main/java/parser/DocReader.java
@@ -40,7 +40,7 @@ public class DocReader {
 				int index = Integer.parseInt(file.getName().substring(
 								0, 
 								file.getName().length()-4));
-				if(index == 2121 || index == 5894 || index == 1583) {
+				if(index == 2121 || index == 5894 || index == 1583 || index == 2455) {
 					continue;
 				}
 				SimulProb simulProb = new SimulProb(index);
