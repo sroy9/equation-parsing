f727d3a31fea3082e9b3a46a2b668f53d7a57e13
Subhro  Roy <subhroroy@Subhros-MacBook-Pro.local>
2015-02-11 10:29:56 -0600

    C

--- src/main/java/semparse/Lexicon.java
+++ src/main/java/semparse/Lexicon.java
@@ -19,31 +19,33 @@ public class Lexicon implements Serializable {
 
 	private static final long serialVersionUID = 6497021707338812080L;
 	Map<String, List<List<String>>> lex;
+	Map<String, List<List<String>>> ccgGroups;
 	
 	public Lexicon() {
 		lex = new HashMap<String, List<List<String>>>();
+		ccgGroups = new HashMap<String, List<List<String>>>();
 	}
 	
 	@Override
 	public String toString() {
 		String str = "";
-		for(String key : lex.keySet()) {
-			str += key + " : " + Arrays.asList(lex.get(key)) + "\n\n\n";
+		for(String key : ccgGroups.keySet()) {
+			str += key + " : " + Arrays.asList(ccgGroups.get(key)) + "\n\n\n";
 		}
 		return str;
 	}
 	
 	public List<List<String>> get(String key) {
-		if(lex.containsKey(key))
-			return lex.get(key);
+		if(ccgGroups.containsKey(key))
+			return ccgGroups.get(key);
 		else 
 			return new ArrayList<>();
 	}
 	
 	public List<List<String>> getAll() {
 		List<List<String>> allPats = new ArrayList<>();
-		for(String key : lex.keySet()) {
-			allPats.addAll(lex.get(key));
+		for(String key : ccgGroups.keySet()) {
+			allPats.addAll(ccgGroups.get(key));
 		}
 		return allPats;
 	}
@@ -98,16 +100,34 @@ public class Lexicon implements Serializable {
  				if(!lexicon.lex.containsKey(pair.getFirst())) {
  					lexicon.lex.put(pair.getFirst(), new ArrayList<List<String>>());
  				}
+ 				System.out.println("Problem index : "+x.problemIndex);
  				List<Pair<String, IntPair>> pattern = 
  						Lexicon.getNodeString(x, y.nodes, pair.getSecond());
+ 				System.out.println("NodeString : "+pattern);
  				List<Pair<String, IntPair>> ccgPattern = 
  						getCCGPattern(pattern, pair.getFirst());
+ 				System.out.println("CCG Pattern : "+ccgPattern+"\n");
  				List<String> ccgOnlyStrings = new ArrayList<>();
  				for(Pair<String, IntPair> pat : ccgPattern) {
  					ccgOnlyStrings.add(pat.getFirst());
  				}
  				if(!contains(lexicon.lex.get(pair.getFirst()), ccgOnlyStrings)) {
  					lexicon.lex.get(pair.getFirst()).add(ccgOnlyStrings);
+ 				}
+ 				
+ 				for(Pair<String, IntPair> pat : ccgPattern) {
+ 					if(!lexicon.ccgGroups.containsKey(pat.getFirst())) {
+ 						lexicon.ccgGroups.put(pat.getFirst(), new ArrayList<List<String>>());
+ 					}
+ 					if(!pat.getFirst().equals("EXPR")) {
+ 						List<String> terms = new ArrayList<>();
+ 						for(int j=pat.getSecond().getFirst(); j<pat.getSecond().getSecond(); ++j) {
+ 							terms.add(x.ta.getToken(j));
+ 						}
+ 						if(!contains(lexicon.ccgGroups.get(pat.getFirst()), terms)) {
+ 							lexicon.ccgGroups.get(pat.getFirst()).add(terms);
+ 						}
+ 					}
  				}
  			}
  		}
@@ -117,21 +137,25 @@ public class Lexicon implements Serializable {
 	public static List<Pair<String, IntPair>> getCCGPattern(
 			List<Pair<String, IntPair>> pattern, String label) {
 		List<Pair<String, IntPair>> ccgPattern = new ArrayList<>();
-		int lastLoc = 0;
 		for(int i=0; i<pattern.size(); ++i) {
-			if(pattern.get(i).equals("EXPR")) {
-				if(lastLoc < i) ccgPattern.add(new Pair<String, IntPair>(
-						"SOMETHING", new IntPair(lastLoc, i)));
+			if(!pattern.get(i).getFirst().equals("EXPR")) {
+				int loc = pattern.size();
+				for(int j=i+1; j<pattern.size(); ++j) {
+					if(pattern.get(j).getFirst().equals("EXPR")) {
+						loc = j;
+						break;
+					}
+				}
+				int start = pattern.get(i).getSecond().getFirst();
+				int end = pattern.get(loc-1).getSecond().getSecond();
+				ccgPattern.add(new Pair<String, IntPair>(
+						"SOMETHING", new IntPair(start, end)));
+				i = loc-1;
+			} else {
 				ccgPattern.add(pattern.get(i));
-				lastLoc = pattern.get(i).getSecond().getSecond();
-				i=lastLoc;
 			}
 		}
-		if(!pattern.get(pattern.size()-1).equals("EXPR")) {
-			ccgPattern.add(new Pair<String, IntPair>("SOMETHING", new IntPair(
-							lastLoc, 
-							pattern.get(pattern.size()-1).getSecond().getSecond())));
-		}
+		System.out.println("Intermediate : "+ccgPattern);
 		for(int i=0; i<ccgPattern.size(); i++) {
 			if(ccgPattern.get(i).getFirst().equals("EXPR")) continue;
 			int prev = 0, next = 0;
@@ -145,14 +169,7 @@ public class Lexicon implements Serializable {
 					next++;
 				}
 			}
-			String ccgLabel = "";
-			for(int j=0; j<prev; j++) {
-				ccgLabel += "EXPR\\";
-			}
-			ccgLabel += label;
-			for(int j=0; j<next; j++) {
-				ccgLabel += "/EXPR";
-			}
+			String ccgLabel = label + "_" + prev + "_" + next;
 			ccgPattern.get(i).setFirst(ccgLabel);
 		}
 		return ccgPattern;
