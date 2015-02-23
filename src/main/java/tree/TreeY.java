069eb584841150845c8041b39c7ca6176b64c00b
Subhro  Roy <subhroroy@wirelessprvnat-172-17-96-64.near.illinois.edu>
2015-02-23 11:44:20 -0600

    C

--- src/main/java/tree/TreeY.java
+++ src/main/java/tree/TreeY.java
@@ -5,7 +5,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import structure.Equation;
 import structure.Node;
@@ -18,17 +20,23 @@ public class TreeY implements IStructure, Serializable {
 	
 	private static final long serialVersionUID = 2399969922362221136L;
 	public Equation equation;
+	public Map<String, List<Integer>> varTokens;
 	
 	public TreeY() {
 		equation = new Equation();
+		varTokens = new HashMap<String, List<Integer>>();
 	}
 	
 	public TreeY(TreeY other) {
-		equation = new Equation();
+		equation = new Equation(other.equation);
+		varTokens = new HashMap<String, List<Integer>>();
+		varTokens.putAll(other.varTokens);
 	}
 	
 	public TreeY(SimulProb prob) {
 		equation = new Equation(prob.equation);
+		varTokens = new HashMap<String, List<Integer>>();
+		varTokens.putAll(m);
 	}
 	
 	public static float getLoss(TreeY y1, TreeY y2) {
