91a02fb9d6ac8acd0f623df252971a91a61c8cbe
Shyam Upadhyay <shyamupa@gmail.com>
2015-02-24 15:55:42 -0600

    bug in lexFeat

--- src/main/java/relevance/RelFeatGen.java
+++ src/main/java/relevance/RelFeatGen.java
@@ -7,7 +7,9 @@ import java.util.List;
 
 import structure.Node;
 import utils.FeatGen;
+import utils.Tools;
 import edu.illinois.cs.cogcomp.core.datastructures.IntPair;
+import edu.illinois.cs.cogcomp.edison.sentences.Sentence;
 import edu.illinois.cs.cogcomp.sl.core.AbstractFeatureGenerator;
 import edu.illinois.cs.cogcomp.sl.core.IInstance;
 import edu.illinois.cs.cogcomp.sl.core.IStructure;
@@ -19,7 +21,8 @@ public class RelFeatGen extends AbstractFeatureGenerator implements
 	
 	private static final long serialVersionUID = 1810851154558168679L;
 	public Lexiconer lm = null;
-	
+	private static int lexWindow=5;
+	private static int posWindow=5;
 	public RelFeatGen(Lexiconer lm) {
 		this.lm = lm;
 	}
@@ -35,10 +38,65 @@ public class RelFeatGen extends AbstractFeatureGenerator implements
 	public static List<String> getFeatures(RelX x, RelY y) {
 		List<String> features = new ArrayList<>();
 		addPOSfeatures(features,x,y,x.quantIndex);
+		addLexfeatures(features,x,y,x.quantIndex);
+		addUnitfeatures(features,x,y,x.quantIndex);
+		addNextTokenfeatures(features,x,y,x.quantIndex);
+//		IsOneAnotherFeature(features,x,y,x.quantIndex);
 		return features;
 	}
+
+	private static void addNextTokenfeatures(List<String> features, RelX x,
+			RelY y, int quantIndex) {
+		
+	}
+
+	private static void addUnitfeatures(List<String> features, RelX x, RelY y,
+			int quantIndex) {
+		String unit = Tools.getUnit(x.quantities.get(x.quantIndex));
+		if(unit.length()!=0)
+		{
+			addFeature("UNIT_"+unit, y, features);
+			if(unit.contains(" per "))
+			{
+				addFeature("UNIT_PER_"+1, y, features);
+			}
+		}
+	}
+
+	private static void addLexfeatures(List<String> features, RelX x, RelY y,
+			int index) {
+		String[] tokens = x.ta.getTokens();
+		int window = lexWindow;
+		int before = Math.max(0, index - window);
+		int after = Math.min(tokens.length- 1, index + window);
+		String __id;
+		String __value;
+
+		String[] forms = new String[before + after + 1];
+		int i = 0;
+		for (int j = before; j <= after; j++) {
+			forms[i++] = tokens[j];
+		}
+
+		for (int j = 0; j < window; j++) {
+			for (i = 0; i < forms.length; i++) {
+				StringBuilder f = new StringBuilder();
+				for (int context = 0; context <= j
+						&& i + context < forms.length; context++) {
+					if (context != 0) {
+						f.append("_");
+					}
+					f.append(forms[i + context]);
+				}
+				__id = ("LEX_"+i + "_" + j);
+				__value = "_" + (f.toString());
+				addFeature(__id + __value,y,features);
+			}
+		}
+	}
+
 	private static void addPOSfeatures(List<String> features, RelX x, RelY y, int index) {
-		int window = 3;
+		int window = posWindow;
 		int before = Math.max(0, index - window);
 		int after = Math.min(x.posTags.size() - 1, index + window);
 		int i;
