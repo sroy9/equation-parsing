911f9329ea3cbd4bd3ec9ba1da7e302c57b16357
sroy9 <sroy9@illinois.edu>
2015-09-16 01:15:42 -0500

    C

--- src/main/java/tree/ConsInfSolver.java
+++ src/main/java/tree/ConsInfSolver.java
@@ -29,10 +29,10 @@ public class ConsInfSolver {
 				new PairComparator<TreeY>() {};
 		MinMaxPriorityQueue<Pair<TreeY, Double>> beam1 = 
 				MinMaxPriorityQueue.orderedBy(pairComparator).
-				maximumSize(200).create();
+				maximumSize(20).create();
 		MinMaxPriorityQueue<Pair<TreeY, Double>> beam2 = 
 				MinMaxPriorityQueue.orderedBy(pairComparator).
-				maximumSize(200).create();
+				maximumSize(20).create();
 		TreeY seed = new TreeY();
 		beam1.add(new Pair<TreeY, Double>(seed, 0.0));
 		
@@ -106,7 +106,6 @@ public class ConsInfSolver {
 		beam1.clear();
 		beam1.addAll(beam2);
 		beam2.clear();
-		
 
 //		beam2.add(beam1.element());
 //		beam1.clear();
@@ -130,10 +129,10 @@ public class ConsInfSolver {
 				new PairComparator<List<Node>>() {};
 		MinMaxPriorityQueue<Pair<List<Node>, Double>> beam1 = 
 				MinMaxPriorityQueue.orderedBy(nodePairComparator)
-				.maximumSize(50).create();
+				.maximumSize(5).create();
 		MinMaxPriorityQueue<Pair<List<Node>, Double>> beam2 = 
 				MinMaxPriorityQueue.orderedBy(nodePairComparator)
-				.maximumSize(50).create();
+				.maximumSize(5).create();
 		int n = y.nodes.size();
 		List<Node> init = new ArrayList<>();
 		init.addAll(y.nodes);
