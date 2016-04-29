720b6df39eeff90b02741d73c50200a0029d6724
sroy9 <sroy9@illinois.edu>
2016-04-29 15:46:26 -0500

    C

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 ## Script to run various configurations of equation parsing framework
-if [ "$#" -eq 0 ]
-then echo "Usage : sh run.sh (Numoccur/Var/Tree/Pipeline/Joint) 
-Pipeline assumes component models to exist"
-    exit 2
-fi
+## if [ "$#" -eq 0 ]
+## then echo "Usage : sh run.sh (Numoccur/Var/Tree/Pipeline/Joint) 
+## Pipeline assumes component models to exist"
+##    exit 2
+## fi
 
 ## Numoccur model
-if [ "$1" = "Numoccur" ]
+if [ "$1" = "" -o "$1" = "Numoccur" ]
 then
     echo "Running Numoccur model"
     java -cp target/classes/:target/dependency/* numoccur.NumoccurDriver 1>log/Numoccur.out
@@ -14,7 +14,7 @@ fi
 
 
 ## Variable Prediction model
-if [ "$1" = "Var" ]
+if [ "$1" = "" -o "$1" = "Var" ]
 then
     echo "Running Var model"
     java -cp target/classes/:target/dependency/* var.VarDriver 1>log/Var.out 
@@ -22,7 +22,7 @@ fi
 
 
 ## Tree Prediction model
-if [ "$1" = "Tree" ]
+if [ "$1" = "" -o "$1" = "Tree" ]
 then
     echo "Running Tree model"
     java -cp target/classes/:target/dependency/* tree.TreeDriver 1>log/Tree.out 
@@ -30,7 +30,7 @@ fi
 
 
 ## Pipeline
-if [ "$1" = "Pipeline" ]
+if [ "$1" = "" -o "$1" = "Pipeline" ]
 then
     echo "Running Pipeline"    
     java -cp target/classes/:target/dependency/* pipeline.PipelineDriver 1>log/Pipeline.out    	
